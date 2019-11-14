d40e2874fe12d03e913209360c47453d8f141fb8
Floriane <floriane@tuta.io>
2019-11-14 14:48:14 -0500

    Implements search functionality

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import LinkList from "./LinkList";
 import CreateLink from "./CreateLink";
 import Header from "./Header";
 import Login from "./Login";
+import Search from "./Search";
 
 const App = () => (
   <div className="center w85">
@@ -13,6 +14,7 @@ const App = () => (
         <Route exact path="/" component={LinkList} />
         <Route exact path="/create" component={CreateLink} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/search" component={Search} />
       </Switch>
     </div>
   </div>
