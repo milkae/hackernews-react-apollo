331e56cd395dcf5b94e254a289668840667cf9cc
Floriane <floriane@tuta.io>
2019-11-14 15:33:58 -0500

    Subscribe to new link and vote

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -26,6 +26,54 @@ export const FEED_QUERY = gql`
   }
 `;
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      createdAt
+      url
+      description
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
 const LinkList = () => {
   const updateCacheAfterVote = (store, createVote, linkId) => {
     const data = store.readQuery({ query: FEED_QUERY });
@@ -34,11 +82,41 @@ const LinkList = () => {
     store.writeQuery({ query: FEED_QUERY, data });
   };
 
+  const subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev;
+        const newLink = subscriptionData.data.newLink;
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+        if (exists) return prev;
+
+        return {
+          ...prev,
+          feed: {
+            ...prev.feed,
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1
+          }
+        };
+      }
+    });
+  };
+
+  const subscribeToNewVotes = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION
+    });
+  };
+
   return (
     <Query query={FEED_QUERY}>
-      {({ loading, error, data }) => {
+      {({ loading, error, data, subscribeToMore }) => {
         if (loading) return <div>Fetching</div>;
         if (error) return <div>Error</div>;
+
+        subscribeToNewLinks(subscribeToMore);
+        subscribeToNewVotes(subscribeToMore);
 
         return (
           <div>
